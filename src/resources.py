273a92b2dbc97f486d0811a99362fbf8aad201fb
Costa-SM <arthur-csm@hotmail.com>
2022-03-23 15:26:50 -0300

    Updated resources.py to import animations

--- src/resources.py
+++ src/resources.py
@@ -1,35 +1,42 @@
+from asyncio.windows_events import NULL
 import os
+from matplotlib import animation
 import pygame as pg
 
 from utils import ASSETS_DIR
 
-def load_sprite(file_name, colorkey = None, scale = 1):
+def load_sprite(folder_name, scale = 1):
     '''
     Function that loads a sprite from a specified file, and returns both the
     image object, and the 
-    :param file_name: file name of the image that will be loaded.
-    :type file_name: string.
-    :param colorkey: 
-    :type colorkey: float.
+    :param folder_name: folder name of the entity that will be loaded.
+    :type folder_name: string.
     :param scale: scale for the image.
     :type scale: float.
+    :rtype: list, rect.
     
     '''
     # Get the full path name and load the image
-    full_name = os.path.join(ASSETS_DIR, file_name)
-    image = pg.image.load(full_name).convert()
+    animation_folders = os.listdir(os.path.join(ASSETS_DIR, folder_name))
+    
+    # Set the list that will have all the animations for the entity
+    animation_set = []
 
-    # Scale the image 
-    size = image.get_size()
-    size = (size[0] * scale, size[1] * scale)
-    image = pg.transform.scale(image, size)
+    # Load the frames into a list, and that list onto another list
+    for folder in animation_folders:
+        current_animation = []
+        for file in os.listdir(folder):
+            current_animation.append(pg.image.load(file).convert())
+            last_index = len(current_animation) - 1
+            
+            # Scale the image
+            size = current_animation[last_index].get_size()
+            size = (size[0] * scale, size[1] * scale)
+            current_animation[last_index] = pg.transform.scale(current_animation[last_index], size)
 
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey, pg.RLEACCEL)
+        animation_set.append(current_animation)
 
-    return image, image.get_rect()
+    return animation_set, animation_set[0][0].get_rect()
 
 
 #def load_sound(file_name):
