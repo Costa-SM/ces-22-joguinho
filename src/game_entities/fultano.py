df63d41be1aa243df0c91203030d5902a02d49a2
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-03-23 16:57:24 -0300

    adding initial movements and & C:/Users/edusi/anaconda3/Scripts/conda.exe run -n ct213 --no-capture-output --live-stream python c:/Users/edusi/OneDrive/Documentos/GitHub/ces-22-joguinho/src/main.py

--- src/game_entities/fultano.py
+++ src/game_entities/fultano.py
@@ -1,6 +1,6 @@
 import os
 import pygame as pg
-from utils import vec
+from utils import *
 from resources import *
 
 class Fultano(pg.sprite.Sprite):
@@ -12,33 +12,35 @@ class Fultano(pg.sprite.Sprite):
         self.health = 100
         self.stepLength = 5
         self.jumpHigh = 10
-        self.pos = vec(100, 600-148)
+        self.pos = vec(100, 600)
         self.speed = vec(0, 0)
-
+        
         # Sprite Loading
-        self.sprites, self.rect = load_sprite('fultano')
-        self.states = ['idle', 'jump', 'run', 'attack']
-        self.currentState = 0
+        self.sprites, self.rect = load_sprite('fultano',2)
+        self.currentState = 'idle'
         self.currentSprite = 0
         self.image = self.sprites[self.currentState][self.currentSprite]
         self.rect = self.image.get_rect()
         self.rect.topleft = [self.pos.x, self.pos.y]
 
-        
-
+        self.running = False
+        self.attacking = False
 
     def update(self):
         '''
         Updates Fultano
         '''
-        self.currentState = 0
+        if not self.running:
+            self.currentState = 'idle'
+        if self.attacking:
+            self.currentState = 'attack_1'
+
         self.currentSprite = self.currentSprite + 0.1
         if self.currentSprite >= len(self.sprites[self.currentState]):
             self.currentSprite = 0        
         self.image = self.sprites[self.currentState][int(self.currentSprite)]
         self.updatePosition()
-
-
+        
     def updatePosition(self):
         '''
         Moves Fultano
@@ -46,16 +48,33 @@ class Fultano(pg.sprite.Sprite):
         key_input = pg.key.get_pressed()
         if (key_input[pg.K_UP]):
             print("cima")
+            self.currentState = 'run'
+            self.running = True
             self.pos.y = self.pos.y - self.jumpHigh
         elif (key_input[pg.K_RIGHT]):
             print("direita")
+            self.currentState = 'run'
+            self.running = True
             self.pos.x = self.pos.x + self.stepLength
         elif (key_input[pg.K_DOWN]):
             print("baixo")
+            self.currentState = 'run'
+            self.running = True
             self.pos.y = self.pos.y + self.jumpHigh
         elif (key_input[pg.K_LEFT]):
             print("esquerda")
+            self.currentState = 'run'
+            self.running = True
+            self.image = pg.transform.flip(self.image, 1, 0)
             self.pos.x = self.pos.x - self.stepLength
+        elif key_input[pg.K_c]:
+            self.attacking = True
+            self.currentState = 'attack_1'
+        else:
+            self.attacking = False
+            self.running = False
+            
+
 
         self.rect.midbottom = self.pos
 
--- src/utils.py
+++ src/utils.py
@@ -9,7 +9,3 @@ BLACK = (0,0,0) # Black color
 
 BASE_PATH = os.path.abspath(os.path.join(os.path.abspath(os.path.join(__file__, os.pardir)), os.pardir))
 ASSETS_DIR = os.path.join(BASE_PATH, 'assets')
-
-IDLE = 0
-JUMP = 1
-RUN = 2
