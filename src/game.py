cfc139d8529c3bcba2c5b698419d6bff7ccd1755
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-03-23 15:31:09 -0300

    desistindo da branch

--- src/game.py
+++ src/game.py
@@ -1,10 +1,12 @@
 from turtle import width
-import pygame
+from matplotlib.pyplot import spring
+import pygame as pg
+from game_entities.fultano import Fultano
 from utils import WIDTH, HEIGHT, BLACK
 
 import rohan
 
-pygame.init()
+pg.init()
 
 class Game():
     def __init__(self) -> None:
@@ -12,11 +14,14 @@ class Game():
         self.initWindow()
 
     def initVariables(self):
-        self.screen = pygame.display
+        self.screen = pg.display
         self.window = self.screen.set_mode((WIDTH,HEIGHT))
-        self.icon = pygame.image
-        self.time = pygame.time
+        self.icon = pg.image
+        self.time = pg.time
         self.running = True
+        self.fultano = Fultano()
+        self.sprites = pg.sprite.Group()
+        self.sprites.add(self.fultano)
 
     def initWindow(self):
         self.screen.set_caption("Game")
@@ -24,21 +29,24 @@ class Game():
       #  self.screen.set_icon(self.icon)
 
         # Set the window color to white
-        self.window.fill(pygame.Color('white'))
+        self.window.fill(pg.Color('white'))
 
         # Create surface for drawing objects
-        self.surface = pygame.Surface((WIDTH, HEIGHT))
+        self.surface = pg.Surface((WIDTH, HEIGHT))
 
     def update(self):
         # Quit Button
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        self.fultano.update()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 self.running = False
         
         self.time.Clock().tick(60)
 
     def render(self):
-        self.screen.flip()
+        self.window.fill(pg.Color('white'))
+        self.sprites.draw(self.window)
+        self.screen.update()
         
 
     
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,15 @@
+import enum
 import os
+import pygame as pg
 
-from numpy import append
+vec = pg.Vector2
 
-WIDTH = 800 # Screen's width
+WIDTH = 1000 # Screen's width
 HEIGHT = 600 # Screen's height
 BLACK = (0,0,0) # Black color
 
 BASE_PATH = os.path.abspath(os.path.join(os.path.abspath(os.path.join(__file__, os.pardir)), os.pardir))
 ASSETS_DIR = os.path.join(BASE_PATH, 'assets')
+
+IDLE = 0
+JUMP = 1
