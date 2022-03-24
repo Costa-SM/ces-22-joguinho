c02511b3bfb7edec1498e8c131391b9535eac0e3
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-03-24 16:42:20 -0300

    adding menu in game titles

--- src/menus.py
+++ src/menus.py
@@ -1,10 +1,16 @@
 import pygame as pg
+from sympy import im
+import os
 from resources import Button
+from utils import FONTS_DIR
 
+fontDir = os.path.join(FONTS_DIR, 'manaspc.ttf')
 
 def main(start, screen, window, clock):
     play = Button(500, 400, ' Play', window)
     window.fill((28, 48, 111))
+    text_img = pg.font.Font(fontDir, 50).render('Fultano\'s Tale', True, 'black')
+    window.blit(text_img, (275, 100))
     while not start:
         if play.draw_button():
             start = True
@@ -16,12 +22,12 @@ def main(start, screen, window, clock):
     
     return start 
 
-
-
 def pause(paused, screen, window, clock):
     resume = Button(500, 400, 'Resume', window)
     while paused:
         window.fill((28, 48, 111))
+        text_img = pg.font.Font(fontDir, 40).render('Game Paused', True, 'black')
+        window.blit(text_img, (350, 200))
         if resume.draw_button():
             paused = False  
         for event in pg.event.get():
