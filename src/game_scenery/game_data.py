4bdd684f53f47696d3889e8423ba80f39fcea8cb
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-03-25 18:57:30 -0300

    adding csv paths

--- src/game_scenery/game_data.py
+++ src/game_scenery/game_data.py
@@ -1,5 +1,4 @@
 level_0 = {
-    #TODO: add .csv files
     'terrain': 'levels/0/level_0_terrain.csv',
     'bg decoration': 'levels/0/level_0_bg_decoration.csv',
     'constraints': 'levels/0/level_0_constraints.csv',
