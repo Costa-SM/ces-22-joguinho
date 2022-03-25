e9dded1f2e7e47e8a4a2247e4a7b5c32f44a9d14
Matheus Ramos <msilvaramos84@gmail.com>
2022-03-25 17:55:13 -0300

    Fixing tilesets

--- tilesets/Enemies.tsx
+++ tilesets/Enemies.tsx
@@ -1,2 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Enemies" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.8" tiledversion="1.8.2" name="Enemies" tilewidth="50" tileheight="48" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image width="50" height="48" source="../assets/skeleton/run/run_1.png"/>
+ </tile>
+</tileset>
