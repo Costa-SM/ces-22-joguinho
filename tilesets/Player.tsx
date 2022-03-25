10d52ae143bf2dd522788443b3b44689c40b04fe
Matheus Ramos <msilvaramos84@gmail.com>
2022-03-25 17:53:41 -0300

    Adding player csv

---
+++ tilesets/Player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="start_end" tilewidth="50" tileheight="50" tilecount="2" columns="2">
+ <image source="../assets/fultano/start-end/start_end.png" width="100" height="50"/>
+</tileset>
