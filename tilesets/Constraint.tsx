74c24b91e48a5b3253cf57d27e4c988d2415cb20
Matheus Ramos <msilvaramos84@gmail.com>
2022-03-25 17:39:05 -0300

    Adding level 0 and other assets

---
+++ tilesets/Constraint.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Constraint" tilewidth="50" tileheight="50" tilecount="1" columns="1">
+ <image source="../assets/terrain/Constraint.png" width="50" height="50"/>
+</tileset>
