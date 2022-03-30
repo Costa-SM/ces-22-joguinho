cf2c942e83450471f6bbf60a58964e4008e4b962
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-03-30 15:55:55 -0300

    adding game over menu

--- src/menus.py
+++ src/menus.py
@@ -1,3 +1,4 @@
+from pdb import Restart
 import pygame as pg
 import os
 from resources import Button
@@ -44,3 +45,25 @@ def pause(paused, screen, clock):
         pg.display.update()
         clock.Clock().tick(60)
     return paused
+
+def death(start, screen, clock):
+    restart = Button(400, 400, 'Main Menu', screen)
+    quit = Button(600, 400, ' Quit', screen)
+    fontDir = 'fonts/manaspc.ttf'
+    while start:
+        screen.fill((36, 37, 77))
+        text_img = pg.font.Font(fontDir, 40).render('Game Over', True, 'black')
+        screen.blit(text_img, (350, 200))
+        quit.draw_button()
+        if restart.draw_button():
+            start = False
+            pg.mixer.music.unpause()
+        for event in pg.event.get():
+            if event.type == pg.QUIT or quit.draw_button():
+                pg.quit()
+            if (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE) or restart.draw_button():    
+                start = False
+                pg.mixer.music.unpause()
+        pg.display.update()
+        clock.Clock().tick(60)
+    return start
