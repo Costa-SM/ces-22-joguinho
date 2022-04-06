21e6dcfed793721dd900539b0a64bf34daeaa1bb
Costa-SM <arthur-csm@hotmail.com>
2022-04-05 21:01:38 -0300

    Restored relative links in utils file

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,4 @@
-import pygame as pg
+import os
 
 TILE_SIZE = 50
 VERTICAL_TILE_NUMBER = 12
@@ -7,3 +7,6 @@ SCREEN_HEIGHT = VERTICAL_TILE_NUMBER * TILE_SIZE
 SCREEN_WIDTH = 1000
 
 FULTANO_HEALTH = 5
+
+BASE_PATH = os.path.abspath(os.path.join(os.path.abspath(os.path.join(__file__, os.pardir)), os.pardir))
+ASSETS_DIR = os.path.join(BASE_PATH, 'assets')
