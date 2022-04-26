07ac8572018ec2e7a6e3ea98a33522e4c5189600
edmsimp <63891357+edmsimp@users.noreply.github.com>
2022-04-26 20:43:46 -0300

    adding fultano and enemy documentation

--- src/game_entities/enemy.py
+++ src/game_entities/enemy.py
@@ -5,47 +5,88 @@ from random import randint
 from utils import BASE_PATH
 
 class Enemy(AnimatedTile):
+    '''
+    Class that represents an enemy.
+    
+    '''
     def __init__(self, size, x, y):
+        '''
+        Enemy class' constructor.
+        :param size: enemy size
+        :type size: int
+        :param x: enemy x coordinate
+        :type x: int
+        :param y: enemy y coordinate
+        :type y: int
+        
+        '''
         super().__init__(size, x, y, os.path.join(BASE_PATH, 'assets/skeleton/walk'))
-        # TODO: refactor this and implement image resize
+        # Set enemy position and speed
         self.rect.y += size - self.image.get_size()[1] + self.image.get_size()[1] / 8
         self.speed = randint(1, 2)
-
         self.rect.height = self.image.get_rect().height - 20
         self.rect.y += 15
-
+        self.previousSpeed = self.speed
+        # Enemy logic
         self.died = False
         self.dying = False
-        self.attacking = False
-        self.previousSpeed = self.speed
+        self.attacking = False        
     
     def move(self):
+        '''
+        Function that moves the enemy.
+
+        '''
         self.rect.x += self.speed
         
     def reverseImage(self):
+        '''
+        Function that reverses the enemy image
+
+        '''
         if self.speed < 0:
             self.image = pg.transform.flip(self.image, True, False)
     
     def reverse(self):
+        '''
+        Function that reverses the enemy velocity
+
+        '''
         self.speed *= -1
         self.previousSpeed *= -1
 
     def attack(self):
+        '''
+        Function for enemy to attack
+
+        '''
+        # Change sprite and speed
         flip = True if self.speed < 0 else False
-        self.previousSpeed = self.speed
-        self.speed = 0
         self.changeState(os.path.join(BASE_PATH, 'assets/skeleton/attack2'), flip, (200, 130))
+        self.previousSpeed = self.speed
+        self.speed = 0        
+        # Change state
         self.attacking = True
     
     def die(self):
+        '''
+        Function for enemy to die
+
+        '''
+        # Change sprite and speed
         flip = True if self.speed < 0 else False
         self.speed = 0
         self.changeState(os.path.join(BASE_PATH, 'assets/skeleton/dead_near'), flip, (100, 96))
+        # Change state
         self.dying = True
     
     def update(self, shift):
-        self.rect.x += shift
+        '''
+        Function that updates the enemy
 
+        '''
+        self.rect.x += shift
+        # State handler
         if self.dying == True and int(self.frameIndex) == len(self.frames) - 1:
             self.died = True
         if self.attacking == True and int(self.frameIndex) == len(self.frames) - 1:
--- src/game_entities/fultano.py
+++ src/game_entities/fultano.py
@@ -4,26 +4,36 @@ from resources import importFolder
 from utils import *
 
 class Fultano(pg.sprite.Sprite):
+    '''
+    Class that represents Fultano.
+    
+    '''
     def __init__(self, pos):
+        '''
+        Fultano class' constructor.
+        :param pos: Fultano position
+        :type pos: tuple
+        
+        '''
         super().__init__()
+        # Sprite initialization
         self.importCharacterAssets()
         self.frameIndex = 0
         self.animationSpeed = 0.1
         self.image = self.animations['idle'][self.frameIndex]
         self.image = pg.transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect(topleft = pos)
+        # Health variables
         self.health = FULTANO_HEALTH
         self.healthSprites = pg.sprite.Group()
+        # Player physics
         self.lastStatus = None
         self.collisionSide = None
-
-		# player movement
         self.direction = pg.math.Vector2(0,0)
         self.speed = 8
         self.gravity = 0.8
         self.jumpSpeed = -16
-
-		# player status
+		# Player logic and state
         self.status = 'idle'
         self.facingRight = True
         self.onGround = False
@@ -32,7 +42,6 @@ class Fultano(pg.sprite.Sprite):
         self.onRight = False
         self.attacking = False
         self.attackType = 'attack_1'
-
         # Player hurt
         self.timeHurted = 10
         self.blinking = False
@@ -40,6 +49,10 @@ class Fultano(pg.sprite.Sprite):
         self.waitHurt = False
 
     def importCharacterAssets(self):
+        '''
+        Function that imports fultano assets.
+
+        '''
         characterPath = ASSETS_DIR + '/fultano/'
         self.animations = {'idle':[],'run':[],'jump':[],'fall':[], 'attack_1':[], 'attack_2':[], 'attack_3':[]}
 
@@ -48,53 +61,52 @@ class Fultano(pg.sprite.Sprite):
             self.animations[animation] = importFolder(fullPath)
 
     def animate(self):
-        if self.lastStatus != self.status:
-            self.frameIndex = 0
-        
-        animation = self.animations[self.status]
+        '''
+        Function that animates Fultano.
 
-        # loop over frame index 
+        '''
+        # Update frame
+        if self.lastStatus != self.status:
+            self.frameIndex = 0        
+        animation = self.animations[self.status]
         self.frameIndex += self.animationSpeed
         if self.frameIndex >= len(animation):
             self.frameIndex = 0
-
+        # Update image
         image = animation[int(self.frameIndex)]
         image = pg.transform.scale(image, (100, 74))
-
+        # If the player is taking damage
         if self.blinking == True:
             self.countHurted += 0.1
             if int(self.countHurted) % 2 == 1 and self.countHurted > 1.3 and not self.waitHurt:
                 image = pg.Surface((100, 74), pg.SRCALPHA, 16)
-
         if int(self.countHurted) >= self.timeHurted:
             self.countHurted = 0
             self.blinking = False
-
+        # If the image needs to be fliped
         if self.facingRight:
             self.image = image
         else:
             flippedImage = pg.transform.flip(image,True,False)
             self.image = flippedImage
-
         self.rect = pg.Rect(self.rect.x, self.rect.y, 50, self.image.get_rect().height)
-
         self.lastStatus = self.status
 
     def getInput(self):
-        keys = pg.key.get_pressed()
+        '''
+        Function that get player keyboard input.
 
+        '''
+        keys = pg.key.get_pressed()
         # If Fultano has been damaged, ignore input and get pushed back
         if (self.collisionSide != None):
             if self.countHurted > 1.3 and self.countHurted < 1.8 and self.blinking:
                 self.direction.x = -self.collisionSide
-
                 if not self.onGround:
-                    self.jump()
-                
-                return
-            
+                    self.jump()                
+                return            
             self.collisionSide = None
-
+        # Key input logic
         if keys[pg.K_RIGHT] and not self.attacking:
             self.direction.x = 1
             self.facingRight = True
@@ -103,10 +115,8 @@ class Fultano(pg.sprite.Sprite):
             self.facingRight = False
         else:
             self.direction.x = 0
-
         if keys[pg.K_UP] and self.onGround:
             self.jump()
-
         if (keys[pg.K_c] or keys[pg.K_f] or keys[pg.K_v]) and self.onGround and self.direction.x == 0:
             self.attacking = True
             if keys[pg.K_c]:
@@ -120,6 +130,10 @@ class Fultano(pg.sprite.Sprite):
                 self.attacking = False 
 
     def getStatus(self):
+        '''
+        Function get player status based on his attributes.
+
+        '''
         if self.attacking:
             self.status = self.attackType
         else:
@@ -134,21 +148,36 @@ class Fultano(pg.sprite.Sprite):
                     self.status = 'idle'
 
     def getHealth(self):
-        
+        '''
+        Function that get player health.
+
+        '''        
         for heart in range(int(self.health)):
             heartSurface = pg.image.load(os.path.join(BASE_PATH, 'assets/interface/heart.png')).convert_alpha()
             sprite = StaticTile(TILE_SIZE, 25 + 30*heart, 20, heartSurface)
             self.healthSprites.add(sprite)
 
     def applyGravity(self):
+        '''
+        Function that apllies gravity in the player
+
+        '''
         self.direction.y += self.gravity
         self.rect.y += self.direction.y
 
     def jump(self):
+        '''
+        Jump function.
+
+        '''
         if self.status != 'jump':
             self.direction.y = self.jumpSpeed
 
     def update(self):
+        '''
+        Function that updates Fultano.
+
+        '''
         self.getInput()
         self.getStatus()
         self.getHealth()
